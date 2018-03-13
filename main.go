4842bc5d448b1fea39f60e4e981c3e240516d17a
Inseok <itchain@gmail.com>
2018-03-14 08:00:18 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func main() {
+
+	bytes, err := ioutil.ReadAll(os.Stdin)
+
+	if err != nil {
+		log.Println("Error to read from stdin")
+	}
+
+	fmt.Print(string(bytes[:]))
+}
